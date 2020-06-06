ffad9696287f2e0e66ee3040b9a26a81593b7140
Viktor Apoyan <viktor@opus.am>
2020-06-06 22:28:28 +0200

    Added Titles to the fragments Added Navigation

--- account/src/main/java/am/chamich/app/account/features/AccountsActivity.kt
+++ account/src/main/java/am/chamich/app/account/features/AccountsActivity.kt
@@ -8,17 +8,25 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import kotlinx.android.synthetic.main.account_activity_accounts.*
 
-internal class AccountsActivity : AppCompatActivity() {
+internal class AccountsActivity : AppCompatActivity(), AppBarConfiguration.OnNavigateUpListener {
 
     private lateinit var progressView: View
+    private lateinit var navController: NavController
 
-    val accountComponent: AccountComponent by lazy(mode = LazyThreadSafetyMode.NONE) {
-        DaggerAccountComponent
-            .builder()
-            .accountModule(AccountModule(applicationContext))
-            .build()
+    val accountComponent: AccountComponent? by lazy(mode = LazyThreadSafetyMode.NONE) {
+        if (ACCOUNT_COMPONENT == null) {
+            // THIS BLOCK IS USED FOR INTEGRATION TESTS ONLY
+            DaggerAccountComponent
+                .builder()
+                .accountModule(AccountModule(applicationContext))
+                .build()
+        } else ACCOUNT_COMPONENT
     }
 
 
@@ -27,8 +35,17 @@ internal class AccountsActivity : AppCompatActivity() {
         setContentView(R.layout.account_activity_accounts)
 
         initializeProgressAndDisableClicks()
+        setupNavController()
         setupActionBar()
     }
+
+    override fun onSupportNavigateUp() =
+        if (R.id.destination_fragment_accounts == navController.currentDestination?.id) {
+            finish()
+            false
+        } else {
+            navController.navigateUp()
+        }
 
     fun showProgress(show: Boolean) {
         progressView.isVisible = show
@@ -39,7 +56,20 @@ internal class AccountsActivity : AppCompatActivity() {
         progressView.setOnTouchListener { _, _ -> true }
     }
 
+    private fun setupNavController() {
+        navController = findNavController(R.id.nav_host_fragment_accounts)
+    }
+
     private fun setupActionBar() {
         setSupportActionBar(toolbar_accounts)
+        setupActionBarWithNavController(
+            navController, AppBarConfiguration.Builder()
+                .setFallbackOnNavigateUpListener(this)
+                .build()
+        )
+    }
+
+    companion object {
+        var ACCOUNT_COMPONENT: AccountComponent? = null
     }
 }
